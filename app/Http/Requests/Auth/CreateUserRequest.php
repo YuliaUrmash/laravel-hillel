fc01c26a9efe24ab5d9eabf8093926b3fb4a6d17
Yulia Urmash <yuliaurmash@gmail.com>
2022-07-26 00:15:28 +0300

    Breeze, Authentication

---
+++ app/Http/Requests/Auth/CreateUserRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Rules\Phone;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        return [
+            'name:min' => 'User name should be more than 2 symbols.',
+            'surname:min' => 'User surname should be more than 2 symbols',
+            'phone' => 'Incorrect phone format.',
+            'password' => 'Incorrect password.'
+        ];
+    }
+    //$request->validate([
+    //'name' => ['required', 'string', 'max:255'],
+    //'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    //'password' => ['required', 'confirmed', Rules\Password::defaults()],
+    //]);
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'min:2', 'max:35'],
+            'surname' => ['required', 'min:2', 'max:50'],
+            'birthdate' => ['required', 'date', 'before_or_equal:-18 years'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone' => ['required', 'string', 'max:15', 'unique:users', new Phone],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ];
+    }
+}
